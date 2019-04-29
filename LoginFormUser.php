fdc3202e39217765adc12910021b763d7be40fc4
Eslam ElAnsary <45523125+EslamAnsary@users.noreply.github.com>
2019-04-29 20:06:52 +0200

    Add files via upload

---
+++ LoginFormUser.php
@@ -0,0 +1,32 @@
+<html>
+<!-- registerForm-->    
+<head>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+             <h2>Login</h2>
+        </div>
+        <form action="Login.php" method="post">
+            
+        <div>  
+        <label for="firstname">First Name</label>
+        <input type="text" name="firstname" required>
+        </div>
+        
+        <div>  
+        <label for="password">Password</label>
+        <input type="password" name="password" required>
+        </div>    
+            
+            
+        <input type="submit" value="Sign In" name="login_form">  
+            
+        
+        </form> 
+        
+        </div>
+    
+    </body>
+</html>
+   
